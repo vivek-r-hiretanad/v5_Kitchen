1497a4823e4f4d13ccf1cd909ec6dd6f37b00fcb
vivek-r-hiretanad <153304872+vivek-r-hiretanad@users.noreply.github.com>
2024-12-04 12:45:27 +0530

    Update Place.jsx

--- frontend/src/pages/PlaceOrder/Place.jsx
+++ frontend/src/pages/PlaceOrder/Place.jsx
@@ -1,134 +1,3 @@
-// import React, { useContext,  useEffect,  useState } from 'react'
-// import './Place.css'
-// import axios from 'axios';
-// import { Storecontext } from '../../context/Storecontext';
-// import { useNavigate } from 'react-router-dom';
-// const PlaceOrder = () => {
-
-
-//   const {getTotalCartamount,token, food_list, cartitem,url}=useContext(Storecontext)
-//   const [data,setData]=useState({
-//     firstname:"",
-//     lastname:"",
-//     email:"",
-//     street:"",
-//     city:"",
-//     state:"",
-//     zipcode:"",
-//     country:"",
-//     phone:""
-//   })
-
-//   const onChangeHandler=(event)=>{
-//     const name=event.target.name;
-//     const value=event.target.value;
-//     setData(data=>({...data,[name]:value}))
-//   }
-
-//   const placeorder=async(event)=>{
-//       event.preventDefault();
-//       let orderItems=[];
-//       food_list.map((item)=>{
-//         if(cartitem[item._id]>0){
-//           let itemInfo=item;
-//           itemInfo["quantity"]=cartitem[item._id];
-//           orderItems.push(itemInfo)
-//         }
-//       })
-//      let orderData={
-//       address:data,
-//       items:orderItems,
-//       amount:getTotalCartamount()+10,
-
-//      }
-
-    
-
-//     let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
-//     if (response.data.success) {
-//         const { session_url } = response.data; // Make sure this contains the URL
-//         window.location.replace(session_url);
-//     } else {
-//         alert("Error");
-//     }
-      
-//   }
-
-//   const navigate=useNavigate();
-
-//   useEffect(()=>{
-//          if(!token){
-//           navigate('/cart');
-//          }
-//          else if(getTotalCartamount()===0){
-//               navigate('/cart')
-//          }
-//   },[token])
-
-
-
-//   return (
-//     <form onSubmit={placeorder} className='place-order'>
-     
-//      <div className="placeorderleft">
-//       <p className="title">Delivery Information</p>
-//       <div className="multifields">
-//         <input  required name="firstname" onChange={onChangeHandler} value={data.firstname} type="text" placeholder='First-Name' />
-//         <input  required name="lastname" onChange={onChangeHandler} value={data.lastname} type="text" placeholder='Last-Name' />
-//       </div>
-//       <input  required name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder='Email addres'/>
-//       <input  required name="street" onChange={onChangeHandler} value={data.street} type="text" placeholder='Street'/>
-     
-//      <div className="multifields">
-//         <input  required name="city" onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
-//         <input  required name="state" onChange={onChangeHandler} value={data.state} type="text" placeholder='State' />
-//       </div>
-//       <div className="multifields">
-//         <input  required name="zipcode" onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip-Code' />
-//         <input  required name="country" onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' />
-//       </div>
-//       <input  required name="phone" onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone Number' />
-//      </div>
-
-//      <div className="placeholderrigth">
-//      <div className="carttotal">
-//             <h2>Cart Total</h2>
-//             <div>
-//             <div className="carttotaldetial">
-//                 <p>
-//                   Subtotal
-//                 </p>
-//               <p>
-//                  ₹{getTotalCartamount()}
-//               </p>
-//               </div>
-//               <hr />
-//               <div className="carttotaldetial">
-//               <p>
-//                   Delivery fee
-//                 </p>
-//               <p>
-//                  ₹{getTotalCartamount()===0?0:10}
-//               </p>
-//               </div>
-//               <hr />
-//               <div className="carttotaldetial">
-//                 <b>Total</b>
-//                 <b> ₹{getTotalCartamount()===0?0:getTotalCartamount()+10}</b>
-//               </div>
-              
-//             </div>
-//             <button type='submit' >Proceed to Payment</button>
-//           </div>
-//      </div>
-//     </form>
-//   )
-// }
-
-// export default PlaceOrder
-
-
-
 import React, { useContext, useEffect, useState } from 'react';
 import './Place.css';
 import axios from 'axios';
@@ -194,23 +63,23 @@ const PlaceOrder = () => {
   return (
     <form onSubmit={placeorder} className='place-order'>
       <div className="placeorderleft">
-        <p className="title">Delivery Information</p>
+        <p className="title">Table  Information</p>
         <div className="multifields">
-          <input required name="firstname" onChange={onChangeHandler} value={data.firstname} type="text" placeholder='First-Name' />
-          <input required name="lastname" onChange={onChangeHandler} value={data.lastname} type="text" placeholder='Last-Name' />
+          <input required name="firstname" onChange={onChangeHandler} value="Table " type="text" placeholder='First-Name'  />
+          <input required name="lastname" onChange={onChangeHandler} value="1" type="text" placeholder='Last-Name' />
         </div>
-        <input required name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder='Email address' />
-        <input required name="street" onChange={onChangeHandler} value={data.street} type="text" placeholder='Street' />
+        <input required name="email" onChange={onChangeHandler} value="Table1@gmail.com" type="email" placeholder='Email address' />
+        <input required name="street" onChange={onChangeHandler} value="Kengeri" type="text" placeholder='Street' />
 
         <div className="multifields">
-          <input required name="city" onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
-          <input required name="state" onChange={onChangeHandler} value={data.state} type="text" placeholder='State' />
+          <input required name="city" onChange={onChangeHandler} value="Bangalore" type="text" placeholder='City' />
+          <input required name="state" onChange={onChangeHandler} value="Karnataka" type="text" placeholder='State' />
         </div>
         <div className="multifields">
-          <input required name="zipcode" onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip-Code' />
-          <input required name="country" onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' />
+          <input required name="zipcode" onChange={onChangeHandler} value="566052" type="text" placeholder='Zip-Code' />
+          <input required name="country" onChange={onChangeHandler} value="India" type="text" placeholder='Country' />
         </div>
-        <input required name="phone" onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone Number' />
+        <input required name="phone" onChange={onChangeHandler} value="0123456"type="text" placeholder='Phone Number' />
       </div>
 
       <div className="placeholderight">
@@ -223,7 +92,7 @@ const PlaceOrder = () => {
             </div>
             <hr />
             <div className="carttotaldetial">
-              <p>Delivery fee</p>
+              <p>Tax</p>
               <p>₹{getTotalCartamount() === 0 ? 0 : 10}</p>
             </div>
             <hr />
@@ -241,3 +110,4 @@ const PlaceOrder = () => {
 };
 
 export default PlaceOrder;
+
